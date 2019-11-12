302f0d9cc9ff711b0c109ac2b8875f2a46bf2d72
Michelle Chis <s1230292@monmouth.edu>
2019-11-11 20:22:04 -0500

    Completed multiplication table.

--- MultiplicationTable/src/MultiplicationTable.java
+++ MultiplicationTable/src/MultiplicationTable.java
@@ -7,19 +7,20 @@ public class MultiplicationTable {
 		
 		System.out.format("      ");
         for(int i = 1; i<=tableSize;i++ ) {
-            System.out.format("%4d",i);
-        }//1st for loop
+
+        }//1st for loop. printing out row
         System.out.println();
-        System.out.println("------------------------------------------");
          
         for(int i = 1 ;i<=tableSize;i++) {
-            // print left most column first
-            System.out.format("%4d |",i);
+
             for(int j=1;j<=tableSize;j++) {
                 System.out.format("%4d",i*j);
-            }//3rd for loop
+                
+            }//3rd for loop. printing out products
+            
             System.out.println();
-        }//2nd for loop
+            
+        }//2nd for loop. printing out column
 		
 	} //main
 
