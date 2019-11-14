888e8b86ed5e58787ffad445c5d61753ff35402f
Michelle Chis <s1230292@monmouth.edu>
2019-11-13 19:49:28 -0500

    Table with lines.

--- MultiplicationTable/src/MultiplicationTable.java
+++ MultiplicationTable/src/MultiplicationTable.java
@@ -7,12 +7,14 @@ public class MultiplicationTable {
 		
 		System.out.format("      ");
         for(int i = 1; i<=tableSize;i++ ) {
-
+        	System.out.format("%4d",i);
         }//1st for loop. printing out row
         System.out.println();
-         
+        System.out.println("______________________________________________");
+        
         for(int i = 1 ;i<=tableSize;i++) {
-
+        	System.out.format("%4d |",i);
+        	
             for(int j=1;j<=tableSize;j++) {
                 System.out.format("%4d",i*j);
                 
